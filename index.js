123130371928769ae3929995a40b3cf48b320723
Ewerton <3w3rt0n@gmail.com>
2018-10-19 16:28:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,56 @@ const express = require('express');
 const http = require('http');
 
 var settings = {
-		port:5000
+		port:1883
 }
 
-var app = express();
-var server = http.createServer(app);
+var http = http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type": "text/html"});
+	if(request.url == "/"){
+		response.write("<h1>Página principal</h1>");
+		console.log('Pagina inicial');
+	}else if(request.url == "/bemvindo"){
+		response.write("<h1>Bem-vindo :)</h1>");
+		console.log('Pagina bem vindo');
+	}else{
+		response.write("<h1>Página não encontrada :(</h1>");
+		console.log('Pagina nao encontrada');
+	}
+	response.end();
+});
 
-var server = new mosca.Server(settings, function() {
+var mqtt = new mosca.Server(settings, function() {
     server.attachHttpServer(app);
 });
 
-server.on('ready', function(){
+mqtt.on('ready', function(){
     console.log("Servidor MQTT em execucao");
+});
+
+mqtt.on('clientConnected', function(client) {
+  console.log('client connected', client.id);
+});
+
+mqtt.on('published', function(packet, client) {
+  console.log('Published : ', packet.payload);
+});
+
+server.on('subscribed', function(topic, client) {
+  console.log('subscribed : ', topic);
+});
+
+mqtt.on('unsubscribed', function(topic, client) {
+  console.log('unsubscribed : ', topic);
+});
+
+mqtt.on('clientDisconnecting', function(client) {
+  console.log('clientDisconnecting : ', client.id);
+});
+
+mqtt.on('clientDisconnected', function(client) {
+  console.log('clientDisconnected : ', client.id);
+});
+
+http.listen(process.env.PORT || 5000, function(){
+	console.log('Servidor HTTP rodando!');
 });
