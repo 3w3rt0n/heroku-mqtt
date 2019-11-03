021e7841fc61313e9bf86e6d1854848041e2b51f
Ewerton <3w3rt0n@gmail.com>
2019-11-03 10:30:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const express = require('express');
 const http = require('http');
 
 var settings = {
-		port:1883
+		port:80
 }
 
-var serverhttp = http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "text/html"});
-	if(request.url == "/"){
-		response.write("<h1>Página principal</h1>");
-		console.log('Pagina inicial');
-	}else if(request.url == "/bemvindo"){
-		response.write("<h1>Bem-vindo :)</h1>");
-		console.log('Pagina bem vindo');
-	}else{
-		response.write("<h1>Página não encontrada :(</h1>");
-		console.log('Pagina nao encontrada');
-	}
-	response.end();
-});
+#var serverhttp = http.createServer(function(request, response){
+#	response.writeHead(200, {"Content-Type": "text/html"});
+#	if(request.url == "/"){
+#		response.write("<h1>Página principal</h1>");
+#		console.log('Pagina inicial');
+#	}else if(request.url == "/bemvindo"){
+#		response.write("<h1>Bem-vindo :)</h1>");
+#		console.log('Pagina bem vindo');
+#	}else{
+#		response.write("<h1>Página não encontrada :(</h1>");
+#		console.log('Pagina nao encontrada');
+#	}
+#	response.end();
+#});
 
 var mqtt = new mosca.Server(settings, function() {
     mqtt.attachHttpServer(serverhttp);
